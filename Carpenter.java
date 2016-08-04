ba841fe9ffcdb063b918b7f9063f9b253ba02f61
KeithBuji <Keith.Saldanha@ust-global.com>
2016-08-04 12:28:57 +0530

    Add a Carpenter file
    This file describes the latest cutting edge software

---
+++ Carpenter.java
@@ -0,0 +1,16 @@
+
+public class Carpenter extends Person
+{
+
+	@Override
+	public void whatDoYouDo() 
+	{
+		System.out.println("Makes Furniture");
+		
+	}
+	public boolean doYouSpeak()
+	{
+		return true;
+	}
+	
+}
